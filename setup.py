de39c779c8fb7cba22a1fc4d1b7e643bd44b804d
AftabSahil <aftabsahil81@gmail.com>
2025-07-30 18:18:48 +0530

    Add .gitignore file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Genrative AI Project',
+    version='0.0.0',
+    author='Sahil Aftab',
+    author_email='aftabsahil81@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    
+)
